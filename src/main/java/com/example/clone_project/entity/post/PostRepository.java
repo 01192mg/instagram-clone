2d548cec71759476d1060429cae754a772cf2ab5
DESKTOP-HUBSSKG\admin <lia.sia72@gmail.com>
2022-08-20 22:19:03 +0900

    gh_deploy.sh

--- src/main/java/com/example/clone_project/entity/post/PostRepository.java
+++ src/main/java/com/example/clone_project/entity/post/PostRepository.java
@@ -9,4 +9,11 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("select p from Post p join fetch p.member m where m.username = :username")
     List<Post> findByUsername(@Param("username") String username);
+
+    List<Post> findAllByOrderByModifiedAtDesc();
+
+    Post findAllById(Long id);
+
+    List<Post> findAllByContentLike (String content);
+
 }
