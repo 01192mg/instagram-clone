f35c9769e13ef044b88e8dc07e3619842d8e445f
01192mg <01192mg@gmail.com>
2022-08-25 10:28:52 +0900

    fix: 게시글 조회 시 작성자 닉네임, 프로필 이미지 추가

--- src/main/java/com/example/clone_project/dto/response/PostResponseDto.java
+++ src/main/java/com/example/clone_project/dto/response/PostResponseDto.java
@@ -13,16 +13,20 @@ import java.util.stream.Collectors;
 public class PostResponseDto {
     private final Long id;
     private final String content;
+    private final String imageUrl;
     private final String author;
+    private final String authorNickname;
+    private final String authorProfileImage;
     private final List<CommentResponseDto> comments;
 
-    private final String imageUrl;
     private final LocalDateTime modifiedAt;
 
     public PostResponseDto(Post post) {
         this.id = post.getId();
         this.content = post.getContent();
         this.author = post.getMember().getUsername();
+        this.authorNickname = post.getMember().getNickname();
+        this.authorProfileImage = post.getMember().getProfileImageUrl();
         this.comments = Optional.ofNullable(post.getComments()).orElseGet(Collections::emptyList).stream().map(CommentResponseDto::fromEntity).collect(Collectors.toList());
         this.imageUrl = post.getImageUrl();
         this.modifiedAt = post.getModifiedAt();
