27d95a51d02a609eedd8da1a1b3785fee1e18586
01192mg <01192mg@gmail.com>
2022-08-25 11:01:35 +0900

    fix: 회원 기본 프로필 이미지 변경

--- src/main/java/com/example/clone_project/service/MemberService.java
+++ src/main/java/com/example/clone_project/service/MemberService.java
@@ -46,7 +46,7 @@ public class MemberService {
             .username(requestDto.getUsername())
             .nickname(requestDto.getNickname())
             .password(passwordEncoder.encode(requestDto.getPassword()))
-            .profileImageUrl("https://play-lh.googleusercontent.com/38AGKCqmbjZ9OuWx4YjssAz3Y0DTWbiM5HB0ove1pNBq_o9mtWfGszjZNxZdwt_vgHo=w240-h480-rw")
+            .profileImageUrl("https://yozm.wishket.com/static/img/default_avatar.png")
             .build();
     memberRepository.save(member);
     return ResponseDto.success(new MemberResponseDto(member));
